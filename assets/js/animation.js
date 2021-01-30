bb48aee04c922aa20b3be5dc8ed42b86bbb3147b
Tobi-dev99 <59740193+Tobi-dev99@users.noreply.github.com>
2021-01-30 15:39:54 +0530

    Added about section
    added new section to my page

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -1,6 +1,10 @@
 const mainPageEl = document.querySelector(".main-page");
 const navbarItem = document.querySelectorAll(".navbar__item");
 const subHeading = document.querySelector(".sub-heading");
+const scrollBarIcon = document.querySelector('.scroll-down');
+
+//featureCard
+const featureCard = $('.feature__card');
 
 //Activating  ScrollTrigger
 gsap.registerPlugin(ScrollTrigger);
@@ -31,12 +35,52 @@ gsap.from(".mountain", {
   ease: "back",
 });
 
+////////////////////////////////////////////////////////////////////
+///ABOUT SECTION
+
+gsap.from('.heading__about', {
+  scrollTrigger: {
+    trigger: '.heading__about',
+    markers: false,
+    toggleActions: "restart reverse restart pause",
+  },
+  xPercent: 50,
+  opacity: 0,
+  duration: 1,
+  ease: 'ease'
+});
+
+gsap.from(".info-box__text", {
+  scrollTrigger: {
+    trigger: ".info-box",
+    markers: false,
+    toggleActions: "restart reverse restart pause",
+  },
+  yPercent: 50,
+  opacity: 0,
+  duration: 1,
+  ease: "ease",
+});
+
+gsap.from(".feature__card", {
+  scrollTrigger: {
+    trigger: ".info-box__animate",
+    markers: false,
+    toggleActions: "restart reverse restart pause",
+  },
+  yPercent: 50,
+  opacity: 0,
+  stagger: 0.1,
+  // duration: .5,
+  // ease: "ease",
+});
+
 //Navbar fixed
 
 ScrollTrigger.create({
   trigger: ".scroll-down",
   start: "top 100px",
-  end: 1000,
+  end: 9999,
   markers: false,
   toggleClass: { className: "navbar--scrolled", targets: ".navbar" },
 });
@@ -49,7 +93,6 @@ ScrollTrigger.create({
 const subHeadingText = document.querySelector(".hide-text");
 
 const sHeadingChar = subHeadingText.textContent.split("");
-console.log(sHeadingChar);
 
 sHeadingChar.forEach((el) => {
   const spanEl = document.createElement("span");
@@ -90,3 +133,38 @@ headingTl
     ease: 'back',
     duration: 1
   })
+
+  //scrollbar-icon
+
+  gsap.to(scrollBarIcon, {yPercent: -100, ease: 'back', yoyo: true, repeat: -1 })
+
+
+  ////////////////////////////////////////////////////////////////////////////////////////////
+  //Feature Card move
+
+
+
+  featureCard.on('mouseenter', function (e) {
+    let thisPage = $(this).attr('id');
+    console.log(thisPage);
+
+     gsap.to($('#'+thisPage), {
+       yPercent: -20,
+       scale: 1.1,
+       ease: "ease",
+       duration: 0.4,
+     });
+  })
+
+   featureCard.on("mouseleave", function (e) {
+     let thisPage = $(this).attr("id");
+
+     gsap.to($("#" + thisPage), {
+       yPercent: 0,
+       scale: 1,
+       ease: "ease",
+       duration: 0.4,
+     });
+   });
+
+
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,95 @@
 //Parallax Components
-const scene = document.querySelector('.scene');
+const scene = document.querySelector(".scene");
 const parallax = new Parallax(scene);
+
+//Navbar
+const NavbarItems = $(".navbar__items");
+
+
+//Navbar Link Animations
+const line = $("<div />").addClass("line");
+
+line.appendTo(NavbarItems);
+
+var active = NavbarItems.find(".navbar__item--active");
+let pos = 0;
+let wid = 0;
+console.log(active);
+
+if (active.length) {
+  pos = active.position().left;
+  wid = active.width();
+  line.css({
+    left: pos,
+    width: wid,
+  });
+  console.log(wid);
+}
+
+console.log(NavbarItems.find(".navbar__link"));
+
+NavbarItems.find(".navbar__link").click(function (e) {
+  e.preventDefault();
+  if (
+    !$(this).parent().hasClass("active") &&
+    !NavbarItems.hasClass("animate")
+  ) {
+    NavbarItems.addClass(".animate");
+
+    var _this = $(this);
+    console.log(NavbarItems.find("li"));
+    NavbarItems.find("li").removeClass("navbar__item--active");
+
+    let position = _this.parent().position();
+    let width = _this.parent().width();
+
+    if (position.left >= pos) {
+      line.animate(
+        {
+          width: position.left - pos + width,
+        },
+        300,
+        function () {
+          line.animate(
+            {
+              width: width,
+              left: position.left,
+            },
+            150,
+            function () {
+              NavbarItems.removeClass("animate");
+            }
+          );
+          _this.parent().addClass("navbar__item--active");
+        }
+      );
+    } else {
+      line.animate(
+        {
+          width: pos - position.left + wid,
+          left: position.left,
+        },
+        300,
+        function () {
+          line.animate(
+            {
+              width: width,
+            },
+            150,
+            function () {
+              NavbarItems.removeClass("animate");
+            }
+          );
+          _this.parent().addClass("navbar__item--active");
+        }
+      );
+    }
+
+    pos = position.left;
+    wid = width;
+  }
+});
+
 
 
 
